acda0a3cac5fa883c073f242dadec23b34256874
zeNn-G <treroin@gmail.com>
2022-11-24 23:31:25 +0300

    IdTable

--- main.cpp
+++ main.cpp
@@ -44,7 +44,40 @@ int main()
         }
     }
 
-    display(table);
+    vector<vector<double>> idTable(taskHalf, vector<double>(taskHour, 0.0));
+
+    for (int i = 0; i < taskHalf; i++)
+    {
+        idTable[i][i] = i + 1;
+    }
+
+    for (int i = 0; i < taskHalf; i++)
+    {
+        for (int j = i + 1; j < taskHalf; j++)
+        {
+            double max = 0;
+            for (int k = i; k < j; k++)
+            {
+                double payment = table[i][k] + table[k + 1][j];
+
+                if (payment > max)
+                    max = payment;
+                idTable[i][j] = k + 1;
+            }
+        }
+    }
+
+    int day = 1;
+    int i = 0;
+    int j = taskHalf - 1;
+    while (i < j)
+    {
+        cout << "On day " << day << " do task with id " << idTable[i][j] << endl;
+        i = idTable[i][j];
+        day++;
+    }
+
+    cout << " Most profitable completion of the task takes " << day << " days " << endl;
 
     return 0;
 }
