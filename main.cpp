af84ed432b0e2d00394bdfbe36f93273ded99698
zeNn-G <treroin@gmail.com>
2022-11-24 23:14:54 +0300

    Pain

--- main.cpp
+++ main.cpp
@@ -1,4 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+template <typename T>
+void display(const vector<T> &vec)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        for (int j = 0; j < vec[i].size(); j++)
+            cout << vec[i][j] << "\t";
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int taskHalf = 5;
+    int taskHour = 5;
+    vector<double> taskPortionPayment = {100, 500, 800, 900, 1000};
+
+    vector<vector<double>> table(taskHalf, vector<double>(taskHour, 0.0));
+
+    for (int i = 0; i < taskHalf; i++)
+    {
+        table[i][i] = taskPortionPayment[i];
+    }
+
+    for (int i = 0; i < taskHalf; i++)
+    {
+        for (int j = i + 1; j < taskHalf; j++)
+        {
+            double max = 0;
+            for (int k = i; k < j; k++)
+            {
+                double payment = table[i][k] + table[k + 1][j];
+
+                if (payment > max)
+                    max = payment;
+            }
+            table[i][j] = max;
+        }
+    }
+
+    display(table);
+
+    return 0;
+}
