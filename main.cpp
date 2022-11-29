68ae1cc2f56c9fd69351124743babd2562b90ab8
zeNn-G <treroin@gmail.com>
2022-11-29 19:07:09 +0300

    Spain without s

--- main.cpp
+++ main.cpp
@@ -5,66 +5,84 @@
 template <typename T>
 void display(const std::vector<T> &vec)
 {
-    for (auto &row : vec)
+    std::cout << "[ ";
+    for (const auto &elem : vec)
+        std::cout << elem << " ";
+    std::cout << "]" << std::endl;
+}
+
+void unboundedKnapsack(const std::vector<float> &payment, std::vector<float> &halfPayments, std::vector<int> &days, int portion)
+{
+    int i = 0, max = 0, c = 0;
+
+    int total = 0;
+    int lastElement;
+
+    // max elements index in halfPayments
+
+    while (portion > 0)
     {
-        for (auto &col : row)
-        {
-            std::cout << col << "\t";
-        }
-        std::cout << std::endl;
+        max = *std::max_element(halfPayments.begin(), halfPayments.end());
+        int index = std::distance(halfPayments.begin(), std::max_element(halfPayments.begin(), halfPayments.end()));
+        lastElement = index + 1;
+
+        days[i] = lastElement;
+
+        halfPayments[index] = 0;
+        portion -= lastElement;
+
+        i++;
+        c++;
+        total += payment[lastElement - 1];
+
+        std::cout << "Day " << c << " : " << days[i - 1] << " task portion(s) with payment value " << payment[lastElement - 1] << " TL" << std::endl;
     }
 }
 
-int unboundedKnapsack(const std::vector<int> &payment, const std::vector<float> &taskPortion, float totalTaskInHours)
+void fillPaymentVector(std::vector<float> &payments, int portion)
 {
-    int n = payment.size();
-    int n2 = taskPortion.size();
+    float hour = 0.5;
 
-    int totalSize = n + n2;
-
-    std::vector<std::vector<int>> dp(n, std::vector<int>(totalSize + 1, 0));
-
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < portion; i++)
     {
-        dp[i][0] = 0;
-    }
+        int payment;
 
-    for (int c = 0; c < totalSize + 1; c++)
+        std::cout << "Enter the payment value (in TL) for task portion ID " << i + 1 << " having length " << hour << " hour(s): ";
+        std::cin >> payment;
+        payments.push_back(payment);
+        hour += 0.5;
+    }
+}
+
+void fillHalfPaymentVector(std::vector<float> &halfPayments, const std::vector<float> &payments, int portion)
+{
+    for (int i = 0; i < portion; i++)
     {
-        dp[0][c] = int(c / taskPortion[0]) * payment[0];
+        int half;
+        half = payments[i] / (i + 1);
+
+        halfPayments.push_back(half);
     }
-
-    int i, j;
-
-    for (i = 1; i < n; i++)
-    {
-        for (j = 0; j < totalSize + 1; j++)
-        {
-            if (taskPortion[i] <= j)
-            {
-                dp[i][j] = std::max(dp[i - 1][j], payment[i - 1] + dp[i][j - taskPortion[i - 1]]);
-            }
-            else
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
-        }
-    }
-
-    display(dp);
-
-    return dp[i][j];
 }
 
 int main()
 {
-    // std::vector<float> taskPortion = {0.5, 1, 1.5, 2, 2.5};
-    // std::vector<int> payment = {100, 500, 800, 900, 1000};
-    float totalTaskInHours = 8;
-    std::vector<float> taskPortion = {1.0, 3.0, 4.0, 5.0};
-    std::vector<int> payment = {15, 50, 60, 90};
+    std::vector<float> halfPayments;
+    std::vector<float> payments;
+    std::vector<int> days = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    std::cout << unboundedKnapsack(payment, taskPortion, totalTaskInHours);
+    int portion;
+    float taskLength;
+
+    std::cout << "Enter the assigned total task length (in half-hour(s)): ";
+    std::cin >> taskLength;
+
+    portion = taskLength * 2;
+
+    fillPaymentVector(payments, portion);
+    fillHalfPaymentVector(halfPayments, payments, portion);
+
+    unboundedKnapsack(payments, halfPayments, days, portion);
 
     return 0;
 }
